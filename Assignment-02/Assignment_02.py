10faf1ff5602e31f7f9d72bf02bb6bacb3531eae
ahayo1 <70355033+ahayo1@users.noreply.github.com>
2020-09-03 12:51:32 -0400

    Update Assignment_02.py

--- Assignment-02/Assignment_02.py
+++ Assignment-02/Assignment_02.py
@@ -1,15 +1,15 @@
 """
-  Prompts a user to enter 10 integers. If the user enters anything other than integers, 
-  remind her that only integers are allowed and let her retry. Don't allow the user to enter more 
-  than 10 or less than 10 integers. Display the 10 integers back to the user at the end. 
-  Calculate the following statistics from the 10 integers entered:
+Prompts a user to enter 10 integers. If the user enters anything other than integers, 
+remind her that only integers are allowed and let her retry. Don't allow the user to enter more 
+than 10 or less than 10 integers. Display the 10 integers back to the user at the end. 
+Calculate the following statistics from the 10 integers entered:
 
-  Minimum
-  Maximum
-  Range
-  Mean
-  Variance
-  Standard Deviation
+Minimum
+Maximum
+Range
+Mean
+Variance
+Standard Deviation
 """
 
 # Ask the users for x number of integers
